ac1c1cd6589c0a9dc8fa92b40dd45c1abb2937da
gowrish9595@gmail.com <gowrish9595@gmail.com>
2022-10-11 06:54:38 +0800

    Q4. Can you model a parrot? We are specifically interested in three parrots, one that lived in a house with dogs one in a house with cats , the other lived on a farm next to the rooster. 1. A parrot living with dogs says: “Woof, woof” 2. A parrot living with cats says: “ Me ow” 3. A parrot living near the rooster says: “ Cock-a-doodle-doo ” 4. How do you keep the parrot maintainable? What if we need another parrot lives near a Duck? Or near a phone that rings frequently?

---
+++ src/main/java/com/example/AnimalApplication/Response.java
@@ -0,0 +1,10 @@
+package com.example.AnimalApplication;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+
+@Data
+public class Response {
+    private String r;
+    private String b;
+}
